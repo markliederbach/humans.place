4ed57af86219a400e2df6f22c15a27cec1f095a9
Mark <10621536+markliederbach@users.noreply.github.com>
2023-11-20 14:55:55 -0600

    basic app framework

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/routes/**/*.{svelte,js}'],
+	plugins: [require('daisyui')],
+	// daisyui: {
+	// 	themes: [
+	// 		{
+	// 			angst: {
+	// 				primary: '#075985',
+	// 				secondary: '#0f766e',
+	// 				accent: '#2dd4bf',
+	// 				neutral: '#374151',
+	// 				'base-100': '#111827',
+	// 				info: '#3ABFF8',
+	// 				success: '#36D399',
+	// 				warning: '#FBBD23',
+	// 				error: '#F87272'
+	// 			}
+	// 		}
+	// 	]
+	// }
+}
+
